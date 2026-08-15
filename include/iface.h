463c8266b44f90688c4d7eb228750861a433d5de
Samuel Ye <sya191@sfu.ca>
2026-08-15 02:52:54 +0000

    Edit comments

--- include/iface.h
+++ include/iface.h
@@ -8,8 +8,8 @@
  */
 typedef struct {
     ssize_t (*write)(int fd, const void *buf, size_t len); // function pointer to a defined write function
-    ssize_t (*read)(int fd, void *buf); // should read exactly one ethernet frame per call
-    int fd; // file descriptor to write to
+    ssize_t (*read)(int fd, void *buf); // read exactly one ethernet frame per call
+    int fd; // file descriptor to read/write to
     uint32_t src_ip;
     uint8_t src_mac[6]; // make sure the LSB of the first byte is 0 for unicast address
 } iface_t;
