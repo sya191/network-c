1edf5228e7698ae876da8a643d7ed70e72b01687
Samuel Ye <sya191@sfu.ca>
2026-08-16 04:08:20 +0000

    Add preliminary function prototypes and structs to ip.h

--- include/ip.h
+++ include/ip.h
@@ -1,3 +1,5 @@
+#define DEFAULT_ENTRIES_SIZ 4 // we just need 3 entries for now (default gateway, subnet, broadcast)
+#include "iface.h"
 #include <stdint.h>
 
 /**
@@ -20,7 +22,46 @@ typedef struct {
 
 #pragma pack(pop)
 
+typedef enum {
+    ETHERNET,
+    LOOPBACK // for simplicity no need to implement for now
+} interface_t;
+
+typedef struct {
+    uint32_t dest; // Destination
+    uint32_t mask; // Mask
+    uint32_t next_hop; // Next-hop
+    interface_t interface; // ETHERNET pretty much always
+} routing_ent_t;
+
 /**
- * Demux function which handles ICMP/TCP/etc.
+ * Routing table for IP module
+ * 
+ * Defaults to size 4 routing entries
  */
-int handle_ip();
+typedef struct {
+    routing_ent_t entries[DEFAULT_ENTRIES_SIZ]; // default = 4
+    int size;
+} routing_table;
+
+// For configuration (DHCP needed for automatic population, for now just manual)
+int delete_routing_entry(uint32_t dest);
+int add_routing_entry(routing_ent_t entry);
+
+/**
+ * Demux function which hands payload to modules above
+ * 
+ * @returns protocol code (TCP, ICMP, etc.) on success, -1 on failure
+ */
+int recv_ip(ip_t *ip_msg, iface_t *interface);
+
+/**
+ * Attempts to send IP packet (may fragment)
+ * 
+ * @returns 0 on success, -1 on failure (upper layer modules must decide what to do)
+ */
+int send_ip(
+    void *payload,
+    uint8_t *protocol,
+    size_t len,
+    iface_t *interface);
